0d399851463b13ca9e980863655c7b11b975f6fa
twichai <twichai4869@gmail.com>
2026-04-25 16:31:48 +0700

    add layout and authentication pages with components for login and registration

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,5 @@
+import { ApplicationShell1 } from "@/components/application-shell1";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return <ApplicationShell1>{children}</ApplicationShell1>;
+}
